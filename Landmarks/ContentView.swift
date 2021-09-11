1067655fed8c3bd1cdcb010f4852b3932aa9ae79
yusinoue108 <yusinoue1008@gmail.com>
2021-09-12 02:05:13 +0900

    initial commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Yus Inoue on 2021/09/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+                .foregroundColor(.black)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
