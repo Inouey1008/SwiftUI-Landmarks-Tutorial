977d648669801ebd1c38b5554ba6a682d7d6262c
yusinoue108 <yusinoue1008@gmail.com>
2021-09-12 02:37:43 +0900

    Creating and Combining Views終了

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  CircleImage
+//
+//  Created by Yus Inoue on 2021/09/12.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
