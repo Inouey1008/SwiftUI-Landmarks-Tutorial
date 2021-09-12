ce11e6e273ed604f2d990285a2f702295d11a8d2
yusinoue108 <yusinoue1008@gmail.com>
2021-09-12 13:47:02 +0900

    Building Lists and Navigation Section6 まで終わった

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,38 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                    .foregroundColor(.black)
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-
-                Divider()
-
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-            
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
@@ -48,4 +17,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+
 }
---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkList.swift
+//  LandmarkList
+//
+//  Created by Yus Inoue on 2021/09/12.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail()) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
